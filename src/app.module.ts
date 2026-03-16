fcc680b55ced4dc64bf66959ec34d845b846e24f
yuuta2s <asmaa.guebba19@gmail.com>
2026-03-17 00:45:10 +0100

    initialisation of project, added users and auth cli, still needs to implement steam auth service

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'MYMDPCTOUT',
+      database: 'gamedex',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // à désactiver en prod
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+})
+export class AppModule {}
